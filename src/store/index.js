701b96a20a2c42de4c2c75aef7f944ad5062b150
Richárd Szoljár <szoljarricsi@gmail.com>
2020-04-03 20:00:48 +0200

    Init redux, redux-thunk, redux-dev-tools.

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
+
+const middlewares = [thunk];
+const middlewareEnhancer = applyMiddleware(...middlewares);
+
+const enhancers = [middlewareEnhancer];
+const composedEnhancers = composeWithDevTools(...enhancers);
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
