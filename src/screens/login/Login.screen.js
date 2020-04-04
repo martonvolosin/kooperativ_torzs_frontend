3fcd301aac4f45f66c377daf99d07df77d2d5932
Milan <neninger.milan@gmail.com>
2020-04-04 17:37:24 +0200

    Login screen ui

--- src/screens/login/Login.screen.js
+++ src/screens/login/Login.screen.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Form } from 'react-bootstrap';
 
+import { useHistory } from 'react-router-dom';
 import { LoginWrapper } from '../../components';
 
 const Login = () => {
   const { t } = useTranslation();
+  const history = useHistory();
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log('submit');
   };
 
+  const handleForgotPassword = () => {
+    console.log('forgot password');
+  };
+
   const renderLoginForm = () => (
-    <div className="container">
-      <Form onSubmit={handleSubmit}>
+    <div className="d-flex align-items-center flex-column justify-content-center h-100">
+      <Form className="w-25" onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>{t('email')}</Form.Label>
           <Form.Control type="email" placeholder={t('emailPlaceholder')} />
@@ -22,14 +28,29 @@ const Login = () => {
         <Form.Group controlId="formBasicPassword">
           <Form.Label>{t('password')}</Form.Label>
           <Form.Control type="password" />
-          <Form.Text className="text-muted">{t('forgotPassword')}</Form.Text>
+          <Button
+            variant="link"
+            onClick={handleForgotPassword}
+            className="text-muted"
+            size="sm"
+          >
+            {t('forgotPassword')}
+          </Button>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" block>
           {t('login')}
         </Button>
+        <Form.Text className="text-muted registerSection">
+          {t('noProfile')}
+        </Form.Text>
+        <Button
+          variant="secondary"
+          onClick={() => history.push('/registration')}
+          block
+        >
+          {t('register')}
+        </Button>
       </Form>
-      <Form.Text className="text-muted">{t('noProfile')}</Form.Text>
-      <Button>{t('register')}</Button>
     </div>
   );
 
