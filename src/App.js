af7d914268d0a824b6aa840532034b899c861676
Milan <neninger.milan@gmail.com>
2020-04-04 11:51:47 +0200

    Navigator implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 
 import { store } from './store';
 
+import Navigator from './navigation/Navigator';
+
 function App() {
-  const { t } = useTranslation();
   return (
     <Provider store={store}>
-      <div className="text-success">
-        <h1>{t('test')}</h1>
-      </div>
+      <Navigator />
     </Provider>
   );
 }
---
+++ src/navigation/PrivateRoute.js
@@ -0,0 +1,1 @@
+// TODO Ha van autentikacio
