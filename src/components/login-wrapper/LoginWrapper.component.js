5626870e7416ddecdc2cb84accb004c1fb45336b
Richárd Szoljár <szoljarricsi@gmail.com>
2020-04-04 16:13:17 +0200

    Fix LoginWrapper style.

--- src/components/login-wrapper/LoginWrapper.component.js
+++ src/components/login-wrapper/LoginWrapper.component.js
@@ -5,7 +5,7 @@ const LoginWrapper = ({ children }) => {
   const { t } = useTranslation();
   return (
     <div className="no-gutters row">
-      <div className="d-none d-md-block col-md-5 leftSidePanel">
+      <div className="d-none d-md-block col-md-4 leftSidePanel">
         <div className="whiteBackgroundContainer">
           <h1 className="text-center leftSideWrapperTitle">{t('title')}</h1>
           <p className="leftSideWrapperParagraph">{t('howToUse')}</p>
@@ -16,7 +16,7 @@ const LoginWrapper = ({ children }) => {
           <p className="leftSideWrapperParagraph">{t('noShameToAsk')}</p>
         </div>
       </div>
-      <div className="col-sm-12 col-md-7">{children}</div>
+      <div className="col-sm-12 col-md-8">{children}</div>
     </div>
   );
 };
